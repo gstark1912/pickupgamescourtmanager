c71b3e2a5557f95cca00c7e5f1b4c64f516ff444
Geerr <gstark1912@gmail.com>
2017-06-14 15:33:03 -0300

    pepe

---
+++ UI/app/src/widgets/gs-client-schedule/gs-client-schedule.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    angular.module('gs.client.schedule')
+    .directive('gsClientSchedule', function () {
+        return {
+            restrict: 'E', //Default in 1.3+
+            scope: {
+                client: '='
+            },
+            templateUrl: "app/src/widgets/gs-client-schedule/gs-client-schedule.html",
+            link: function (scope, element, attrs) {
+                var list = element.find("ul");
+            },
+            controller: ['$scope', function ($scope) {
+                $scope.demo1 = {
+                    min: 06,
+                    max: 24
+                };
+            }]
+        };
+    });
+}());
